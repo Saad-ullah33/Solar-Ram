224b5566aa62e39866e698585518651ea47a9c58
Saad-ullah33 <msaadullah541@gmail.com>
2025-08-05 21:44:19 +0500

    Initial commit with legacy peer deps config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./Redux/store";
+import store from "./Redux/Store.js";
 import ErrorBoundary from "./Components/Common/ErrorBoundary";
 import VerifyEmail from "./Components/Common/VerifyEmail.jsx";
 import {
